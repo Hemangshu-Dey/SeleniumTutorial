6d03de547bb899b5cad41e4de3e3fcd54b81fac7
Hemangshu-Dey <hemangshu1904@gmail.com>
2025-10-26 10:57:27 +0530

    creating page object model

---
+++ src/test/java/part2/com/saucedemo/base/BaseTest.java
@@ -0,0 +1,30 @@
+package part2.com.saucedemo.base;
+
+import com.saucedemo.pages.BasePage;
+import com.saucedemo.pages.LoginPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+public class BaseTest {
+    protected WebDriver driver;
+    protected BasePage basepage;
+    protected LoginPage loginPage;
+    private String url = "https://www.saucedemo.com";
+
+    @BeforeClass
+    public void setup() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(url);
+        basepage = new BasePage();
+        basepage.setDriver(driver);
+        loginPage = new LoginPage();
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
+}
